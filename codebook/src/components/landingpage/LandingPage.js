6f30e75c378b468df97f6adea8f5182bdacc3357
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-01-26 23:21:04 +0100

    Create LandingPage.js

---
+++ codebook/src/components/landingpage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandingPage = () => {
+    return (
+        <div className="landingpage-wrapper" >
+            <div className="landingpage-inner-wrapper">
+            <h1 className="landingpage-header">Code-Book</h1>
+            <p className="landingpage-text">Discuss Code. Engage in exchange of inovative ideas. Find the most recent best practices and never miss out on new tech.</p>
+            <h2 className="landingpage-sm-header">We Welcome you to Code-Book. The wet dream of a coder.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
