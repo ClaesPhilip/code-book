b86eff5647ec470387db3224275861e72c2a7b6b
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-01-28 23:06:43 +0100

    Update LandingPage.js

--- codebook/src/components/landingpage/LandingPage.js
+++ codebook/src/components/landingpage/LandingPage.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import './LandingPage.css'
+import React from 'react';
+import './LandingPage.css';
+
+import Jon from '../../images/jon.jpg';
+import Dude from '../../images/dude.jpg';
 
 const LandingPage = () => {
     return (
         <div className="landingpage-wrapper" >
             <div className="landingpage-inner-wrapper">
-            <h1 className="landingpage-header">Code-Book</h1>
+            {/* <h1 className="landingpage-header">Code-Book</h1> */}                
+                <ul className="img-container">
+                    <li>
+                        <img src={Jon} alt="jon" className="jon-logo"/>
+                    </li>
+                    <li>
+                        <img src={Dude} alt="jon" className="dude-logo"/>
+                    </li>
+                </ul>
+
             <p className="landingpage-text">Discuss Code. Engage in exchange of inovative ideas. Find the most recent best practices and never miss out on new tech.</p>
             <h2 className="landingpage-sm-header">We Welcome you to Code-Book. The wet dream of a coder.</h2>
             </div>
